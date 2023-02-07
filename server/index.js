b3e76f3149b0d9b3c00fe7717169f6a8ee9ff756
omerboran22 <omerboran22@gmail.com>
2023-02-07 19:51:03 +0300

    commit

--- server/index.js
+++ server/index.js
@@ -2,10 +2,14 @@ import bodyParser from "body-parser";
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import path from "path";
 import dotenv from "dotenv";
 import customerRoutes from "./routes/customers.js";
 import userRoutes from "./routes/userRouter.js";
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app=express();
 const server=express();
 dotenv.config();
@@ -16,13 +20,16 @@ app.use(bodyParser.urlencoded({limit:"30mb",extended:true}))
 app.use(cors());
 app.disable("x-powered-by");
 
-app.get("/",(req,res)=>{
-  res.json({bos:"naber"});
-})
 
 app.use("/customers",customerRoutes)
 app.use("/user",userRoutes)
+
 server.use("/api/v1",app);
+server.use(express.static(path.resolve(__dirname, '../client/build')));
+server.use("/",function(req,res){
+  res.sendFile(path.resolve(__dirname, 'client', 'build'));
+})
+
 const PORT=process.env.PORT || 5000;
 
 mongoose
